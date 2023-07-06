600b8de16d56d5131fcd0ec35becd39a09c4cf95
ThomasAnselmetto <t.anselmetto@gmail.com>
2023-07-06 11:10:54 +0200

    creazione entita' e relazioni base MtoM

---
+++ src/main/java/org/lesson/fotoportfolio/model/Category.java
@@ -0,0 +1,43 @@
+package org.lesson.fotoportfolio.model;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    @ManyToMany(mappedBy = "categories")
+    private List<Photo> photos = new ArrayList<>();
+
+    //Getters & Setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Photo> getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(List<Photo> photos) {
+        this.photos = photos;
+    }
+}
--- src/main/java/org/lesson/fotoportfolio/model/Photo.java
+++ src/main/java/org/lesson/fotoportfolio/model/Photo.java
@@ -1,5 +1,65 @@
 package org.lesson.fotoportfolio.model;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "photos")
 public class Photo {
-//
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Integer id;
+@Column(nullable = false)
+private String title;
+private String url;
+private boolean visible;
+@ManyToMany
+@JoinTable(name = "photo_category",
+joinColumns = @JoinColumn(name = "photo_id"),
+inverseJoinColumns = @JoinColumn(name = "category_id"))
+private List<Category> categories = new ArrayList<>();
+
+//Getters & Setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
 }
